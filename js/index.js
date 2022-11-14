7ecd193d39fd7cbc16c7483eb648b8c51ceda27b
VINCIDAO <106922708+VINCIDAO@users.noreply.github.com>
2022-11-14 15:18:26 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -130,7 +130,7 @@ async function addUserDecision(event) {
 function openPopupD() {
     var allIds = {};
     if(window.location.host === "vinci-onboarding-repos.github.io") {    
-        href = window.location.host + window.location.host.split('/')[1];
+        href = window.location.origin + '/' + window.location.pathname.split('/')[1];
     } else {
         href = window.location.origin;
     }
@@ -172,7 +172,7 @@ function openPopupT() {
     var allIds = {};
     var href = '';
     if(window.location.host === "vinci-onboarding-repos.github.io") {    
-        href = window.location.host + window.location.host.split('/')[1];
+        href = window.location.origin + '/' + window.location.pathname.split('/')[1];
     } else {
         href = window.location.origin;
     }
